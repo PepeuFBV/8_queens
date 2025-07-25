78e93f27f96670f8ffa13ce236c15a9075f5c413
Pedro Figueira <pedrofigueiraboaviagem@gmail.com>
2025-07-25 01:02:53 -0300

    feat: approach selection

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # Build all .c files in src/ into an executable named 8_queens.out
-gcc src/*.c -o 8_queens.out
+gcc -O2 src/*.c -o 8_queens.out
 
 # Check if build succeeded
 if [ $? -eq 0 ]; then
     echo "Build successful. Running program..."
-    ./8_queens.out
+    if [ -z "$1" ]; then
+        echo "No arguments provided. BRUTE_FORCE will be used as default."
+        ./8_queens.out
+    else
+        ./8_queens.out "$1"
+    fi
 else
     echo "Build failed."
 fi
--- src/main.c
+++ src/main.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include "board.h"
 #include "solver.h"
 
-int main() {
+int main(int argc, char *argv[]) {
     clock_t start, end;
     double cpu_time;
+    SolverApproach approach = BRUTE_FORCE; // default
+
+    if (argc > 1) {
+        if (strcmp(argv[1], "BRUTE_FORCE") == 0) {
+            approach = BRUTE_FORCE;
+        } else if (strcmp(argv[1], "BACKTRACKING") == 0) {
+            approach = BACKTRACKING;
+        }
+    }
 
     start = clock();
 
     resetBoard();
-    bruteForce();
-    printf("Brute force search completed.\n");
+    solveQueens(approach);
 
     end = clock();
     cpu_time = ((double)(end - start)) / CLOCKS_PER_SEC;
